68192928a3ce536538d72d2974c55bcf68b2fed2
Nicholas Vitale <nicholasvitale@Nicholass-MacBook-Pro.local>
2021-01-31 17:01:37 -0800

    First Commit

---
+++ Core/Inc/debye.h
@@ -0,0 +1,5 @@
+//This header file is just to include some of the commonly passed variables from one file to another. MAKE SURE THESE VAIRBALES ARE NOT STATIC IN THEIR DEFINITION
+
+extern uint8_t rxdata;
+extern uint32_t rxlen;
+
